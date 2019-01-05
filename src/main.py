8a6f5a88019aea61a38443660118f099125c4573
ShradhaHolani <30635042+ShradhaHolani@users.noreply.github.com>
2019-01-06 00:35:55 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-import globalPluginHandler,tones	#?? What all functions are dependent on this module
-from globalCommands import commands	#?? What all functions are dependent on this module
-from logHandler import log	#?? What all functions are dependent on this module
+import globalPluginHandler,tones	#Since this is a globinPlugin, we need to import globalPluginHandler,tones- to hear the beeps
+from globalCommands import commands	#All the NVDA functions are defined in this file
+from logHandler import log	#We need this to write to log file of NVDA
 import os	#?? What all functions are dependent on this module
 from parseCommands import *	#?? What all functions are dependent on this module
 
@@ -8,15 +8,15 @@ from parseCommands import *	#?? What all functions are dependent on this module
 #?? What is overall objective of this class? What are inputs and what are outputs?
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
-	#?? What is overall objective of this function? What are inputs and what are outputs?	
+	# The routine to do when the global plugin loads.
 	def __init__(self,*args,**kwargs): 
 		super(GlobalPlugin, self).__init__(*args,**kwargs) 
 		log.debug("Hello world")
 		
 
-	#?? What is overall objective of this function? What are inputs and what are outputs?
+	#The function associated with the key "W"
 	def script_doBeep(self, gesture):
-		tones.beep(440, 1000) # Beep a standard middle A for 1 second. ?? What is standard middle A? What are the input parameters of the function: 440, 1000?		start()					#defined in PythonParser.py file
+		tones.beep(440, 1000) # Beep a tone for 1 second. ? What are the input parameters of the function: 440, 1000?		start()					#defined in PythonParser.py file
 		
 		
 
@@ -24,5 +24,4 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	__gestures={
 		"kb:W":"doBeep"			#Associates key "W" with the function doBeep
 	}
-	#commands.script_review_nextLine(None) ??
 	
