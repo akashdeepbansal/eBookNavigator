4f1319fa4a4528a7e6307d9f1839ee11eeecb125
ShradhaHolani <30635042+ShradhaHolani@users.noreply.github.com>
2019-01-06 17:32:36 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 import globalPluginHandler,tones	#Since this is a globinPlugin, we need to import globalPluginHandler,tones- to hear the beeps
 from globalCommands import commands	#All the NVDA functions are defined in this file
 from logHandler import log	#We need this to write to log file of NVDA
-import os	#?? What all functions are dependent on this module
-from parseCommands import *	#?? What all functions are dependent on this module
+import os	#to call OS related functions
+from parseCommands import *	#to call the start function defined in parseCommands file
 
-
-#?? What is overall objective of this class? What are inputs and what are outputs?
+#The main class where your plugin code needs to be written
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	# The routine to do when the global plugin loads.
